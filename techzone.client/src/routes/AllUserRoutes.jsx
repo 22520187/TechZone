646f38abf589bdf6d641298634f01a1774c9bdbf
22520187 <22520187@gm.uit.edu.vn>
2025-09-30 09:53:13 +0700

    feat: ForgotPassword page

--- techzone.client/src/routes/AllUserRoutes.jsx
+++ techzone.client/src/routes/AllUserRoutes.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/User/Home";
 import NotFound from "../pages/NotFound/NotFound";
 import Login from "../pages/Auth/Login";
+import SignUp from "../pages/Auth/SignUp";
+import ForgorPassword from "../pages/ForgotPassword/Forgotpassword";
 
 const AllUserRoutes = () => {
     return (
@@ -9,6 +11,8 @@ const AllUserRoutes = () => {
             <Route path="/" element={<Home />} />
             <Route path="*" element={<NotFound />} />
             <Route path="/auth/login" element={<Login />} />
+            <Route path="/auth/signup" element={<SignUp />} />
+            <Route path="/auth/forgot-password" element={<ForgorPassword />} />
         </Routes>
     )
 }
