8e4ee9988cce4223365240718987eb88d914861b
Nguyễn Tuấn Lộc ( aka Johan) <nguyenloc24022005@gmail.com>
2025-10-29 08:35:30 +0700

    Feat: Add customer management dashboard and CRUD operations

--- techzone.client/src/routes/AllAdminRoutes.jsx
+++ techzone.client/src/routes/AllAdminRoutes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "../pages/Admin/Dashboard";
 import Products from "@/pages/Admin/Products";
+import Customers from "@/pages/Admin/Customer/Customer";
 
 const AllAdminRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/products" element={<Products />} />
+            <Route path="/customers" element={<Customers />} />
 
             {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
--- techzone.client/src/store.js
+++ techzone.client/src/store.js
@@ -1,13 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./features/AxiosInstance/Auth/Auth";
+import authReducer from "@/features/AxiosInstance/Auth/Auth";
 import productReducer from "@/features/Admin/Products/Product";
-import categoryReducer from "./features/Admin/Categories/Category";
-
+import categoryReducer from "@/features/Admin/Categories/Category";
+import userReducer from "@/features/Admin/Users/User";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         product: productReducer,
         category: categoryReducer,
+        user: userReducer,
     },
 });
