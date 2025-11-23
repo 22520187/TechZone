fa60e561f8cec75a77e8d72fcf7f3e0f0e0b8199
Nguyễn Tuấn Lộc ( aka Johan) <nguyenloc24022005@gmail.com>
2025-11-23 09:54:03 +0700

    Feat(BE): Add controllers, dtos for customer and update missing sql repository of product and category

---
+++ TechZone.Server/Models/DTO/GET/CustomerDetailProductDTO.cs
@@ -0,0 +1,41 @@
+using TechZone.Server.Models.Domain;
+
+namespace TechZone.Server.Models.DTO.GET
+{
+    public class CustomerDetailProductDTO
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public string LongDescription { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public int? StockQuantity { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public virtual BrandDTO? Brand { get; set; }
+
+        public virtual CategoryDTO? Category { get; set; }
+
+        public ICollection<ProductImageDTO> ProductImages { get; set; } = new List<ProductImageDTO>();
+        public ICollection<ProductColorDTO> ProductColors { get; set; } = new List<ProductColorDTO>();
+        public ICollection<ReviewDTO> Reviews { get; set; } = new List<ReviewDTO>();
+        public decimal Rating { get; set; }
+        public int ReviewCount { get; set; }
+
+        private ICollection<CustomerProductPromotionDTO> Promotions { get; set; } = new List<CustomerProductPromotionDTO>();
+
+        public decimal? DiscountPercentage => Promotions
+         .Where(p => p.StartDate <= DateTime.UtcNow && p.EndDate >= DateTime.UtcNow)
+         .Select(p => p.DiscountPercentage)
+         .DefaultIfEmpty(0)
+         .Max();
+
+        public decimal SalePrice => DiscountPercentage.HasValue ? Price * (1 - DiscountPercentage.Value / 100) : Price;
+    }
+}
---
+++ TechZone.Server/Repositories/SQLCategoryRepository.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using TechZone.Server.Models;
+using TechZone.Server.Models.Domain;
+
+namespace TechZone.Server.Repositories.Implement
+{
+    public class SQLCategoryRepository : TechZoneRepository<Category>, ICategoryRepository
+    {
+        public SQLCategoryRepository(TechZoneDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<List<Category>> AdminGetAllCategoryAsync()
+        {
+            return await dbContext.Categories
+                .Include(p => p.Products)
+                .ToListAsync();
+        }
+
+        public async Task<Category> AddCategoryAsync(Category category)
+        {
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> DeleteCategoryAsync(int categoryId)
+        {
+            return await DeleteAsync(p => p.CategoryId == categoryId);
+        }
+
+        public async Task<Category?> UpdateCategoryAsync(int categoryId, Category updatedCategory)
+        {
+            var existingCategory = await dbContext.Categories.FirstOrDefaultAsync(p => p.CategoryId == categoryId);
+            if (existingCategory == null)
+            {
+                return null;
+            }
+            existingCategory.CategoryName = updatedCategory.CategoryName;
+            existingCategory.Description = updatedCategory.Description;
+            
+            await dbContext.SaveChangesAsync();
+            return existingCategory;
+        }
+    }
+}
