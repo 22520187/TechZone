da334c719d0c2145fec7700263c64112344c4586
22520187 <22520187@gm.uit.edu.vn>
2025-11-28 21:16:15 +0700

    update

--- TechZone.Server/obj/Debug/net8.0/TechZone.Server.AssemblyInfo.cs
+++ TechZone.Server/obj/Debug/net8.0/TechZone.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechZone.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c0972dd86eeaaa0c717b3b46771be174cc9ecd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb0e29ebcfa4f4b69180df0d2193c6f68eafdbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechZone.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechZone.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
